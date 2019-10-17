1bfae077fa309f80e0896046eb84b619e23f52b9
ritohirade <rittonboy89love@gmail.com>
2019-10-17 15:12:28 +0900

    bksew

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+  attachment :profile_image
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :introduction, presence: true, length: { maximum: 50 }
 end
